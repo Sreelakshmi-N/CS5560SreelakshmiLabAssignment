12afa896136690702bbcfe026f1b82a37fbdccc5
Sreelakshmi-N <sree25.lakshmi@gmail.com>
2017-07-12 15:16:43 -0500

    commit

---
+++ Lab1A/Scalaprogram/build.sbt
@@ -0,0 +1,8 @@
+name := "Scalaprogram"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq("co.fs2" %% "fs2-core" % "0.9.6",
+  "co.fs2" %% "fs2-io" % "0.9.6")
---
+++ Lab1A/Scalaprogram/src/main/scala/sample.scala
@@ -0,0 +1,20 @@
+/**
+  * Created by sreel on 6/11/2017.
+  */
+import scala.io.StdIn.{readLine, readInt}
+import scala.math._
+import scala.collection.mutable.ArrayBuffer
+import java.io.PrintWriter
+import scala.io.Source
+
+object sample {
+
+  def main(args: Array[String]){
+    val name = "Sree"
+    val age = 22
+    val weight = 165
+
+    println(s"This is $name")
+    println(f"I am ${age} and weigh $weight%dlb")
+  }
+}
